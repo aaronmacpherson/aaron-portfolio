7d26d30872a4ee69b9b598e46b3954012eb90ff0
aaron <aaronmacpherson@outlook.com>
2025-01-02 15:55:39 -0400

    setup tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors : {
+        mainBackground: "#F9FAFB",
+        subBackground: "#F3F4F7",
+        subBackgroundBorder: "#D9D9D9",
+        accent: '#54A251',
+        mainText: "#333333",
+        subText: "#7A7A7A",
+      },
+      backgroundImage: {
+        homeImage: 'url("/images/home-image.jpg")'
+      }
     },
   },
   plugins: [],
