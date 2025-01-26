939338a3694a1ffc58f99c59b29761daa3c4def9
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-01-26 16:57:07 -0400

    changed navigation links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,31 +1,30 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Navigation() {
     return (
         <nav className="flex fixed w-screen items-center justify-center bg-white/90 text-mainText text-xs lg:text-base">
             <ul className="flex flex-wrap">
                 <li>
-                    <a className="flex items-center justify-center md:w-full p-4 text-subText hover:text-black" href="#">
+                    <Link className="flex items-center justify-center md:w-full p-3 text-subText hover:text-black text-xl font-thin" href="#home">
                         Home
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a className="flex items-center justify-center md:w-full p-4 text-subText hover:text-black" href="#about">
+                    <Link className="flex items-center justify-center md:w-full p-3 text-subText hover:text-black text-xl font-thin" href="#about">
                         About
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a className="flex items-center justify-center md:w-full p-4 text-subText hover:text-black" href="#skills">
+                    <Link className="flex items-center justify-center md:w-full p-3 text-subText hover:text-black text-xl font-thin" href="#skills">
                         Skills
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a className="flex items-center justify-center md:w-full p-4 text-subText hover:text-black" href="#web-projects">
-                        Web Projects
-                    </a>
-                </li>
-                <li>
-                    <a className="flex items-center justify-center md:w-full p-4 text-subText hover:text-black" href="#other-projects">
-                        Other Projects
-                    </a>
+                    <Link className="flex items-center justify-center md:w-full p-3 text-subText hover:text-black text-xl font-thin" href="#projects">
+                        Projects
+                    </Link>
                 </li>
             </ul>
         </nav>
