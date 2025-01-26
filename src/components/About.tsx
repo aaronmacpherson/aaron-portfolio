8e7b4a82521020b8792dc08c1d36eee0477f2b96
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-01-26 16:56:20 -0400

    fixed text colour on about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,8 +6,8 @@ export default function About() {
                     <h2 className="flex flex-col items-center text-4xl font-bold after:border-b-8 after:rounded-sm after:w-44 after:block after:border-accent after:relative after:bottom-4 after:z-[-1]">About</h2>
                 </div>
                 <div className="flex flex-col w-3/4 items-center justify-center">
-                    <img className="w-32 h-32 rounded-full mb-4 text-mainText" src="./images/about-image.jpg" alt="picture of aaron macpherson" />
-                    <p className="w-4/5">
+                    <img className="w-32 h-32 rounded-full mb-4" src="./images/about-image.jpg" alt="picture of aaron macpherson" />
+                    <p className="w-4/5 text-mainText">
                         I am currently a second year IT Web Programming student at the Nova Scotia Community College Truro campus. My goal during this program is to graduate in 2025 with a robust skill-set that will enable me to pursue a career in computer programming or full-stack web development. Before entering my current program, I also gained valuable IT skills while taking the Computer Programmer/Analyst course at Conestoga College. My time there helped me secure a 1 year co-op placement at Manulife Financial where I was able to utilize my programming skills to gain real world experience in the web development field.
                     </p>
                 </div>
