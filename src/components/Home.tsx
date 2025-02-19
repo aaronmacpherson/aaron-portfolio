4db771dd1a5e25918d86d2d067d27b85f549a92c
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-02-19 10:38:01 -0400

    switched to html entities on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <section id="home">
             <div className="flex flex-col absolute justify-center items-center px-10 h-[629px] w-screen bg-center bg-cover bg-no-repeat bg-black/50">
                 <p className="text-white text-base sm:text-2xl md:text-4xl md:w-3/4 font-bold">
-                    Hi, I'm <span className="text-accent">Aaron</span>. I'm an aspiring full-stack web developer located in Nova Scotia. I'm currently pursuing any opportunities that will put my development skills to the test.
+                    Hi&#44; I&#39;m <span className="text-accent">Aaron</span>. I&#39;m an aspiring full-stack web developer located in Nova Scotia. I&#39;m currently pursuing any opportunities that will put my development skills to the test.
                 </p>
                 <div className="flex gap-2 mt-5 md:mt-12">
                     <a className="flex flex-nowrap gap-1 text-xs items-center sm:text-base md:text-2xl p-4 bg-black text-white border border-black hover:border-white rounded" href="https://www.linkedin.com/in/a-macpherson/" target="_blank">
