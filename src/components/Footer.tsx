dbbf3e63f6f686037d01e7b008026e0a3cb7799b
Aaron MacPherson <aaronmacpherson@outlook.com>
2025-02-19 10:28:04 -0400

    cleaned up footer, adjusted responsiveness on skills section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
             <footer className="flex flex-col sm:flex-row bg-white sm:justify-between items-center h-[150px] px-10">
                 <div className="sm:flex w-[50px] h-[60px] hidden invisible"></div>
                 <div className="flex flex-col items-center mt-4 sm:mt-0">
-                    <a className="text-subText text-base sm:text-xl border-b-2" href="https://github.com/aaronmacpherson/web-design-final-project" target="_blank">This websites source code</a>
                     <p className="text-subText text-sm">Aaron MacPherson &#169;2024</p>
                 </div>
                 <a className="flex items-center justify-center mt-4 sm:mt-0 w-[40px] h-[50px] sm:w-[50px] sm:h-[60px] bg-accent rounded hover:bg-accent/90" href="#">
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,21 +5,21 @@ export default function Skills() {
                 <div>
                     <h2 className="flex flex-col items-center text-4xl font-bold after:border-b-8 after:rounded-sm after:w-44 after:block after:border-accent after:relative after:bottom-4 after:z-[-1]">Skills</h2>
                 </div>
-                <div className="flex flex-col lg:flex-row w-3/4 items-center justify-center text-nowrap gap-2">
+                <div className="flex flex-col lg:flex-row w-3/4 items-center justify-center lg:text-nowrap gap-2">
                     <div className="flex flex-col p-20 w-3/4 items-center justify-center rounded border border-subBackgroundBorder">
                         <div><i className="fas fa-solid fa-display text-4xl"></i></div>
-                        <div className="text-2xl">Web Development</div>
-                        <div className="text-mainText">HTML, CSS, JavaScript, Next.js, ASP.NET</div>
+                        <div className="text-xl lg:text-2xl">Web Development</div>
+                        <div className="text-sm lg:text-base text-mainText">HTML, CSS, JavaScript, Next.js, ASP.NET</div>
                     </div>
                     <div className="flex flex-col p-20 w-3/4 items-center justify-center rounded border border-subBackgroundBorder">
                         <div><i className="fas fa-solid fa-code text-4xl"></i></div>
-                        <div className="text-2xl">Application Development</div>
-                        <div className="text-mainText">C#, Java, Python</div>
+                        <div className="text-xl lg:text-2xl">Application Development</div>
+                        <div className="text-sm lg:text-base text-mainText">C#, Java, Python</div>
                     </div>
                     <div className="flex flex-col p-20 w-3/4 items-center justify-center rounded border border-subBackgroundBorder">
                         <div><i className="fas fa-solid fa-database text-4xl"></i></div>
-                        <div className="text-2xl">Database Development</div>
-                        <div className="text-mainText">MySQL, MongoDB, Postgres</div>
+                        <div className="text-xl lg:text-2xl">Database Development</div>
+                        <div className="text-sm lg:text-base text-mainText">MySQL, MongoDB, Postgres</div>
                     </div>
                 </div>
             </section>
